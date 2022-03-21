0e3a7520fdb1b491335706cdce958b796fc3cf53
Vimal Raj <8166149+Vimalraj571@users.noreply.github.com>
2022-03-21 13:38:47 +0530

    Basic Flow need to add more

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-const a = 0;
+console.log("APP.js");
+
+const endeCoder = document.querySelector("#en-de-code");
+
+endeCoder.addEventListener("click", () => {
+  console.log("THIS IS CLICK");
+  console.log(window.location.href);
+  const urlComponents = new URL(window.location.href);
+  window.location.href = `./deen-coders/deen-coders.html`;
+});
