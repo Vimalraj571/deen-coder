63da9b807497922225ffb9496d9f257a13369312
Vimal Raj <8166149+Vimalraj571@users.noreply.github.com>
2022-03-23 01:34:54 +0530

    Need to add functions for balance 4

---
+++ numberbase/numberbase.js
@@ -0,0 +1,17 @@
+console.log("Number Base Converter");
+
+const inputBox = document.querySelector("#input-box");
+const outputBox = document.querySelector("#output-box");
+const submitBtn = document.querySelector("#submit-btn");
+const selectOption = document.querySelector("#select-option");
+
+let tempInput = "";
+let selectedOption = "";
+
+inputBox.addEventListener("change", (e) => {
+  tempInput = e.target.value;
+});
+
+selectOption.addEventListener("change", (e) => {
+  selectedOption = e.target.value;
+});
