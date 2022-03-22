5951b38082da6e185a9100e3a46b7ac8f81b6807
Vimal Raj <8166149+Vimalraj571@users.noreply.github.com>
2022-03-22 14:10:59 +0530

    Added the Encode/Decode Complete

--- deen-coders/deen-coder.js
+++ deen-coders/deen-coder.js
@@ -10,12 +10,22 @@ console.log("Encode/Decode");
 
 const encoder = (input) => encodeURIComponent(input);
 const decoder = (input) => decodeURIComponent(input);
-const base64Encode = (input) => Buffer.from(input).toString("base64");
-const base64Decode = (input) => Buffer.from(input, "base64").toString("ascii");
-const md5 = (input) => hashing("md5", input);
-const sha1 = (input) => hashing("sha1", input);
-const sha256 = (input) => hashing("sha256", input);
-const sha512 = (input) => hashing("sha512", input);
+const base64Encode = (input) => {
+  const wordArray = CryptoJS.enc.Utf8.parse(input);
+  const base64 = CryptoJS.enc.Base64.stringify(wordArray);
+  return base64;
+};
+const base64Decode = (input) => {
+  const parsedWordArray = CryptoJS.enc.Base64.parse(input);
+  const parsedStr = parsedWordArray.toString(CryptoJS.enc.Utf8);
+  return parsedStr;
+};
+// const base64Encode = (input) => Buffer.from(input).toString("base64");
+// const base64Decode = (input) => Buffer.from(input, "base64").toString("ascii");
+const md5 = (input) => CryptoJS.MD5(input);
+const sha1 = (input) => CryptoJS.SHA1(input);
+const sha256 = (input) => CryptoJS.SHA256(input);
+const sha512 = (input) => CryptoJS.SHA512(input);
 
 const inputBox = document.querySelector("#input-box");
 const outputBox = document.querySelector("#output-box");
@@ -37,9 +47,21 @@ selectOption.addEventListener("change", (e) => {
 submitBtn.addEventListener("click", () => {
   if (selectedOption === "") {
     alert("Select At least One");
-  } else if (selectedOption === "encode") {
+  } else if (selectedOption === "encodeuri") {
     outputBox.value = encoder(tempInput);
-  } else {
+  } else if (selectedOption === "decodeuri") {
     outputBox.value = decoder(tempInput);
+  } else if (selectedOption === "encodebase64") {
+    outputBox.value = base64Encode(tempInput);
+  } else if (selectedOption === "decodebase64") {
+    outputBox.value = base64Decode(tempInput);
+  } else if (selectedOption === "hashmd5") {
+    outputBox.value = md5(tempInput);
+  } else if (selectedOption === "sha1") {
+    outputBox.value = sha1(tempInput);
+  } else if (selectedOption === "sha256") {
+    outputBox.value = sha256(tempInput);
+  } else if (selectedOption === "sha512") {
+    outputBox.value = sha512(tempInput);
   }
 });
--- hex-rgb/hex-rgb.js
+++ hex-rgb/hex-rgb.js
@@ -1,7 +1,24 @@
-console.log("Encode/Decode URI");
+console.log("Hex/RGB");
 
-const encoder = (input) => encodeURIComponent(input);
-const decoder = (input) => decodeURIComponent(input);
+const toRgb = (inputArr) => {
+  const r = parseInt(inputArr[1] + inputArr[2], 16);
+  const g = parseInt(inputArr[3] + inputArr[4], 16);
+  const b = parseInt(inputArr[5] + inputArr[6], 16);
+  return `${r},${g},${b}`;
+};
+
+// const util = (color) => {
+//   const hexadecimal = color.toString(16);
+//   return hexadecimal.length === 1 ? `0${hexadecimal}` : hexadecimal;
+// };
+
+// const toHex = (inputArr) =>
+//   `#${util(inputArr[0])}${util(inputArr[1])}${util(inputArr[2])}`;
+
+function toHex(r, g, b) {
+  console.log(r+''+g+''+b);
+  return `#${((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1)}`;
+}
 
 const inputBox = document.querySelector("#input-box");
 const outputBox = document.querySelector("#output-box");
@@ -24,8 +41,13 @@ submitBtn.addEventListener("click", () => {
   if (selectedOption === "") {
     alert("selct atleast one");
   } else if (selectedOption === "hex") {
-    outputBox.value = encoder(tempInput);
+    outputBox.value = toHex(tempInput);
   } else {
-    outputBox.value = decoder(tempInput);
+    // console.log(tempInput[1] + tempInput[2],tempInput[3] + tempInput[4],tempInput[5] + tempInput[6]);
+    outputBox.value = toRgb(
+      tempInput[1] + tempInput[2],
+      tempInput[3] + tempInput[4],
+      tempInput[5] + tempInput[6]
+    );
   }
 });
