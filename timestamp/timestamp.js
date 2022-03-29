a99250eea8aeabc54e5c228710a5584e0777fc63
Vimal Raj <8166149+Vimalraj571@users.noreply.github.com>
2022-03-29 20:15:34 +0530

    Added some more

--- timestamp/timestamp.js
+++ timestamp/timestamp.js
@@ -1,5 +1,19 @@
 console.log("TimeStamp Converter");
 
+const dateToEpoc = (input) => {
+  const tempDateObj = new Date(0);
+  tempDateObj.setUTCSeconds(input);
+  return {
+    year: tempDateObj.getFullYear(),
+    month: tempDateObj.getMonth() + 1,
+    date: tempDateObj.getDate(),
+    hours: tempDateObj.getHours(),
+    minutes: tempDateObj.getMinutes(),
+    seconds: tempDateObj.getSeconds(),
+  };
+};
+const epocToDate = () => {};
+
 const inputBox = document.querySelector("#input-box");
 const outputBox = document.querySelector("#output-box");
 const submitBtn = document.querySelector("#submit-btn");
@@ -14,4 +28,19 @@ inputBox.addEventListener("change", (e) => {
 
 selectOption.addEventListener("change", (e) => {
   selectedOption = e.target.value;
+});
+
+submitBtn.addEventListener("click", () => {
+  if (selectedOption === "") {
+    alert("selct atleast one");
+  } else if (selectedOption === "dateToEpoc") {
+    outputBox.value = dateToEpoc(tempInput);
+  } else {
+    const tempInArray = [];
+    const temp = ["Date", "Month", "Year", "Hour", "Minutes", "Second"];
+    for (let i = 0; i < temp.length; i += 1) {
+      tempInArray.push(tempIn);
+    }
+    outputBox.value = epocToDate(tempInput);
+  }
 });
